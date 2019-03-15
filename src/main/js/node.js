c67f0fde89f05d475492135c7bf4d16ad28504f5
kazuhiko.arase <kazuhiko.arase@gmail.com>
2019-03-15 23:52:23 +0900

    commit

---
+++ src/main/js/node.js
@@ -0,0 +1,13 @@
+//
+// node-glue
+// @author Kazuhiko Arase
+// Licensed under MIT LICENSE
+//
+
+'use strict';
+
+!function(prlib) {
+  if (typeof exports === 'object') {
+    module.exports = prlib;
+  }
+}(window.prLib || (window.prLib = {}) );
